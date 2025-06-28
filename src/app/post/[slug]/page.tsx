300b31ee4a3c98d646c29c42be49ba10d3652f5a
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-06-28 16:37:56 -0300

    feat: Criando a pesquisa por slug e pegando isso nas rotas dinâmicas.

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,11 +1,21 @@
+import { findPostBySlugCached } from "@/lib/post/queries";
+import { notFound } from "next/navigation";
+
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
+  let post;
+  try {
+    post = await findPostBySlugCached(slug);
+  } catch {
+    post = undefined;
+  }
+  if (!post) notFound();
   return (
     <h1 className="text-center text-7xl font-bold py-16">
-      Rota Dinâmica: {slug}
+      Rota Dinâmica: {post.title}
     </h1>
   );
 }
