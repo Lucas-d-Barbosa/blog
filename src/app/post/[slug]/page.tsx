e942ba3bb74635bda1ff1c39f2ee44cc9a576a75
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-06-28 16:46:46 -0300

    fix: Melhorando o redirecionamento para notFound e a query por slug.

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,18 +1,12 @@
 import { findPostBySlugCached } from "@/lib/post/queries";
-import { notFound } from "next/navigation";
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
-  let post;
-  try {
-    post = await findPostBySlugCached(slug);
-  } catch {
-    post = undefined;
-  }
-  if (!post) notFound();
+  const post = await findPostBySlugCached(slug);
+
   return (
     <h1 className="text-center text-7xl font-bold py-16">
       Rota Dinâmica: {post.title}
